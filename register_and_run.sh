035e83c8dacce2b9237519cc04f8d0c2e52f48d3
Michal Kubenka <mkubenka@gmail.com>
2020-04-02 17:06:29 +0200

    Fix typo.

--- register_and_run.sh
+++ register_and_run.sh
@@ -137,7 +137,7 @@ else
 fi
 
 # If $HOST and $PORT0 are defined, export METRICS_SERVER env
-if [ ! -z ${HOST+x} ] && [ ! -z ${PORT0+x} ]; then
+if [ ! -z ${HOST} ] && [ ! -z ${PORT0} ]; then
     export LISTEN_ADDRESS="$HOST:$PORT0"
 fi
 
